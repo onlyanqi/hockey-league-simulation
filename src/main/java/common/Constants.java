053b2793ed2b7dfe912e387db624bd02e3ffcd82
Pratheep Kumar Manoharan <kumarmpk@dal.ca>
2020-09-26 21:28:55 -0300

    pulled latest changes of develop branch

---
+++ src/main/java/common/Constants.java
@@ -0,0 +1,13 @@
+package common;
+
+public class Constants {
+
+    public static final String dbFile = ".test.properties";
+    public static final String dbUserName = "DBUserName";
+    public static final String dbPassword = "DBPassword";
+    public static final String dbHost = "DBHost";
+    public static final String dbName = "DBName";
+    public static final String dbPort = "DBPort";
+    public static final String semiColon = ":";
+    public static final String forwardSlash = "/";
+}
