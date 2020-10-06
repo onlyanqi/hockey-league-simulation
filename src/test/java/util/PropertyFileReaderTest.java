7a128ba637de10f0dbfdb8300f83f17287fce79a
Pratheep Kumar Manoharan <kumarmpk@dal.ca>
2020-10-06 18:13:49 -0300

    timezone in DB connection string is added and property reader exception call is changed in DB call and property reader

--- src/test/java/util/PropertyFileReaderTest.java
+++ src/test/java/util/PropertyFileReaderTest.java
@@ -1,22 +1,29 @@
 package util;
 
 import common.Constants;
+import org.junit.Rule;
 import org.junit.Test;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 public class PropertyFileReaderTest {
 
-    @Test(expected = Test.None.class)
-    public void loadPropertyFileNoExceptionTest(){
+    @Test
+    public void loadPropertyFileNoExceptionTest() throws Exception{
         PropertyFileReader read = new PropertyFileReader();
         read.loadPropertyFile(Constants.dbFile);
     }
 
-    @Test(expected = Test.None.class)
+    @Test
     public void loadPropertyFileNotNullTest(){
         PropertyFileReader read = new PropertyFileReader();
-        read.loadPropertyFile(Constants.dbFile);
-        assertNotNull(read.loadPropertyFile(Constants.dbFile));
+        try{
+            read.loadPropertyFile(Constants.dbFile);
+            assertNotNull(read.loadPropertyFile(Constants.dbFile));
+        } catch (Exception e){
+            assertNull(e);
+        }
+
     }
 
 }
