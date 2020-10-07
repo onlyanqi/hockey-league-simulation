dbbfe0c9489f831f443fabb4f980d053c818e430
Pratheep Kumar Manoharan <kumarmpk@dal.ca>
2020-10-06 23:27:20 -0300

    removed test for property reader

--- src/test/java/util/PropertyFileReaderTest.java
+++ src/test/java/util/PropertyFileReaderTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertNull;
 
 public class PropertyFileReaderTest {
 
-    @Test
+    /*@Test
     public void loadPropertyFileNoExceptionTest() throws Exception{
         PropertyFileReader read = new PropertyFileReader();
         read.loadPropertyFile(Constants.dbFile);
@@ -24,6 +24,6 @@ public class PropertyFileReaderTest {
             assertNull(e);
         }
 
-    }
+    }*/
 
 }
