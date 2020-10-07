52f166a5477dbd9a8ba74f89320c3d0c6784fd6e
Mani Teja Varma <mn947410@dal.ca>
2020-10-07 13:16:50 -0300

    Update message in internal state machine

--- src/main/java/state/InternalState.java
+++ src/main/java/state/InternalState.java
@@ -12,20 +12,19 @@ public class InternalState implements IHockeyState {
 
     @Override
     public void entry() {
-        //Parse JSON
-        System.out.println("Internal State -> Entry ");
+
+        System.out.println("Simulating Season");
     }
 
     @Override
     public void process() {
-        //Instantiate and configure LOM
-        System.out.println("Internal State -> Process ");
+
+
     }
 
     @Override
     public IHockeyState exit() {
-        //Persist to DB and move to next state
-        System.out.println("Internal State -> Exit Method ");
+
         return null;
     }
 }
