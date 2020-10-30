16c6432a718955bd1112970e91c5bbd74708754a
Mani Teja Varma <mn947410@dal.ca>
2020-10-30 00:56:32 -0300

    change regular season date

--- src/main/java/simulation/state/PersistState.java
+++ src/main/java/simulation/state/PersistState.java
@@ -1,7 +1,11 @@
 package simulation.state;
 
+import db.data.*;
+import simulation.factory.*;
 import simulation.model.*;
 
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 
 public class PersistState implements ISimulateState{
@@ -17,16 +21,125 @@ public class PersistState implements ISimulateState{
     @Override
     public ISimulateState action() {
         System.out.println("Saving league to DB");
+        saveToPersistence(league);
         return exit();
+    }
+
+    private void saveToPersistence(League league) {
+
+        if(todayIsStartOfSeason()){
+            //persistLeagueToDB();
+        }else{
+            updateDataBaseWithSimulatedDate();
+        }
+
+    }
+
+    private void updateDataBaseWithSimulatedDate() {
+    }
+
+    private void persistLeagueToDB() {
+        if (league != null) {
+            league.setCreatedBy(hockeyContext.getUser().getId());
+            Season season = new Season();
+            season.setName(String.valueOf(LocalDate.now().getYear()));
+
+            try {
+                LeagueConcrete leagueConcrete = new LeagueConcrete();
+                ILeagueFactory addLeagueFactory = leagueConcrete.newAddLeagueFactory();
+                league.addLeague(addLeagueFactory);
+                int leagueId = league.getId();
+
+                SeasonConcrete seasonConcrete = new SeasonConcrete();
+                ISeasonFactory addSeasonDao = seasonConcrete.newAddSeasonFactory();
+                season.addSeason(addSeasonDao);
+                int seasonId = season.getId();
+
+                if (leagueId != 0 && seasonId != 0) {
+                    if (league.getFreeAgent() != null) {
+                        int freeAgentId = addFreeAgent(leagueId, seasonId);
+                        addPlayerList(0, freeAgentId, seasonId, league.getFreeAgent().getPlayerList());
+                    }
+
+                    if (league.getConferenceList() != null && !league.getConferenceList().isEmpty()) {
+                        ConferenceConcrete conferenceConcrete = new ConferenceConcrete();
+                        IConferenceFactory addConferenceDao = conferenceConcrete.newAddConferenceFactory();
+                        for (Conference conference : league.getConferenceList()) {
+                            conference.setLeagueId(leagueId);
+                            conference.addConference(addConferenceDao);
+                            int conferenceId = conference.getId();
+
+                            for (Division division : conference.getDivisionList()) {
+                                DivisionConcrete divisionConcrete = new DivisionConcrete();
+                                IDivisionFactory addDivisionDao = divisionConcrete.newAddDivisionFactory();
+
+                                division.setConferenceId(conferenceId);
+                                division.addDivision(addDivisionDao);
+                                int divisionId = division.getId();
+
+                                for (Team team : division.getTeamList()) {
+                                    TeamConcrete teamConcrete = new TeamConcrete();
+                                    ITeamFactory addTeamDao = teamConcrete.newAddTeamFactory();
+
+                                    team.setDivisionId(divisionId);
+                                    team.addTeam(addTeamDao);
+                                    int teamId = team.getId();
+                                    addPlayerList(teamId, 0, seasonId, team.getPlayerList());
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+            }catch(SQLException sqlException){
+                System.out.println("Unable to save the league! Please try again" + sqlException.getMessage());
+                System.exit(1);
+                sqlException.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private int addFreeAgent(int leagueId, int seasonId) throws Exception {
+        FreeAgentConcrete freeAgentConcrete = new FreeAgentConcrete();
+        IFreeAgentFactory freeAgentDao = freeAgentConcrete.newAddFreeAgentFactory();
+        FreeAgent freeAgent = league.getFreeAgent();
+        freeAgent.setSeasonId(seasonId);
+        freeAgent.setLeagueId(leagueId);
+        freeAgent.addFreeAgent(freeAgentDao);
+        return freeAgent.getId();
+    }
+
+    private void addPlayerList(int teamId, int freeAgentId, int seasonId, List<Player> playerList) throws Exception {
+        if(playerList != null && !playerList.isEmpty()) {
+            PlayerConcrete playerConcrete = new PlayerConcrete();
+            IPlayerFactory addPlayerDao = playerConcrete.newAddPlayerFactory();
+            for (Player player : playerList) {
+                player.setTeamId(teamId);
+                player.setFreeAgentId(freeAgentId);
+                player.setSeasonId(seasonId);
+                addPlayerDao.addPlayer(player);
+            }
+        }
+    }
+
+    private Boolean todayIsStartOfSeason(){
+        if(league.getCurrentDate().equals(LocalDate.of(LocalDate.now().getYear(),10,01))){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     private ISimulateState exit() {
         if(stanleyCupWinnerDetermined()){
             List<TeamScore> teamScoreList = league.getActiveTeamStanding().getTeamsScoreList();
-            for(TeamScore teamscore : teamScoreList){
-                if(teamscore.getNumberOfWins() ==1 ){
-                    System.out.println(teamscore.getTeamName() +" won the stanley cup!");
-                }
+            if(teamScoreList.get(0).getNumberOfWins() > teamScoreList.get(1).getNumberOfWins()){
+                System.out.println(teamScoreList.get(0).getTeamName() +" won the stanley cup!");
+            }else{
+                System.out.println(teamScoreList.get(1).getTeamName() +" won the stanley cup!");
             }
 
             return null;
